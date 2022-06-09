d26f0ea48f79ef027548f58290c3439b1e715d3d
Fernando Gomes <nandinhogoomes@gmail.com>
2022-06-08 22:59:19 -0300

    Criação da classe Order

---
+++ src/main/java/com/projeto/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.course.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    
+}
